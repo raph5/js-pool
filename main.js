84094023053fd2838c1ff7da44d3f9f364c25a49
raph5 <raphguyader@gmail.com>
2023-04-05 15:45:42 +0200

    randerer setup

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+import Game from './lib/game'
+import { Renderer } from './lib/renderer'
 import './style.css'
 
 
+const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("poolCanvas"))
+const renderer = new Renderer(canvas)
+const game = new Game(renderer)
+
+renderer.render()

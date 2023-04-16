83ea44af2936d36e27937d86b6f59bb2d0e16ba0
raph5 <raphguyader@gmail.com>
2023-04-16 20:51:43 +0200

    ajout de commentaires

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ import { Renderer } from './lib/renderer'
 import './style.css'
 
 
+// setup game
 const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("poolCanvas"))
 const renderer = new Renderer(canvas)
 const game = new Game(renderer)
 
-function r() {
+
+function render() {
   requestAnimationFrame(() => {
     game.render()
-    r()
+    render()
   })
 }
 
-r()
+render()

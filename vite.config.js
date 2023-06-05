b6aba6019e32930b894cfe6d334e6b22a20ee821
raph5 <raphguyader@gmail.com>
2023-06-05 18:22:21 +0200

    stick fix and vite config update

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from "vite"
 import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
+	base: 'https://raph.github.io/js-pool/',
+})
+
+
+/*
+Signle page config
+
+export default defineConfig({
 	plugins: [ viteSingleFile() ],
 })
+*/
